116ac43b9e106356bbf408c8b87f294a081a890f
d33psky <d33psky@users.noreply.github.com>
2020-11-14 23:34:42 +0100

    allow some fields to be NULL because the weatherstation does not provide these values

--- db/Hally_table_lucht.sql
+++ db/Hally_table_lucht.sql
@@ -9,16 +9,16 @@ CREATE TABLE `lucht` (
   `datetime` datetime NOT NULL DEFAULT current_timestamp(),
   `luchtvocht` double NOT NULL,
   `luchtdruk` double NOT NULL,
-  `windspeed` double NOT NULL,
-  `hoogtebewolking` double NOT NULL,
-  `fijnstofmeting` double NOT NULL,
-  `windrichting` text NOT NULL
+  `windspeed` double DEFAULT NULL,
+  `hoogtebewolking` double DEFAULT NULL,
+  `fijnstofmeting` double DEFAULT NULL,
+  `windrichting` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `lucht`
 --
 
-INSERT INTO `lucht` (`datetime`, `luchtvocht`, `luchtdruk`, `windspeed`, `hoogtebewolking`, `fijnstofmeting`, `windrichting`) VALUES
-('2020-08-13 22:10:10', 20, 20, 20, 20, 20, 'zuidoost'),
-('2020-08-13 22:10:10', 20, 20, 20, 20, 20, 'zuidoost');
+-- INSERT INTO `lucht` (`datetime`, `luchtvocht`, `luchtdruk`, `windspeed`, `hoogtebewolking`, `fijnstofmeting`, `windrichting`) VALUES
+-- ('2020-08-13 22:10:10', 20, 20, 20, 20, 20, 'zuidoost'),
+-- ('2020-08-13 22:10:10', 20, 20, 20, 20, 20, 'zuidoost');
--- db/Hally_table_temperatuur.sql
+++ db/Hally_table_temperatuur.sql
@@ -8,6 +8,6 @@
 CREATE TABLE `temperatuur` (
   `datetime` datetime NOT NULL DEFAULT current_timestamp(),
   `tempvalue` double NOT NULL,
-  `tempgevoel` double NOT NULL,
+  `tempgevoel` double DEFAULT NULL,
   `dauwpunt` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
